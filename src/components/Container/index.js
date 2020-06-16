ffea99a0592f6255d974e2a2ce297e9fe5c22c32
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2020-06-16 14:51:28 -0300

    :art: Removing all statefull components from the project

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,49 +1,19 @@
-import React, { PureComponent } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { connect } from 'react-redux' 
 
 // COMPONENTS
 import SearchCEP from '../Content';
 
 // ACTIONS CREATORS
-import { updateAddress } from '../../redux-flow/reducers/ZipCode/action-creator'
+import { fetchAddress } from '../../redux-flow/reducers/ZipCode/action-creator'
 
-class Search extends PureComponent {
-  state = {
-    isLoading: false,
-    erro: false,
-  };
-
-  handleSubmit = async (e) => {
-    e.preventDefault();
-    const cepValue = e.target.cep.value;
-
-    if(cepValue.length !== 8){
-      this.setState({erro: true});
-      return;
-    }
-    
-    this.setState({ isLoading: true })
-    try{
-      const cep = cepValue;
-      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-      this.props.setAddress(response.data);
-
-    }catch(err) {
-      this.setState({erro: true});
-    }
-    this.setState({ isLoading: false })
-  }
-
-  render() {
-    return (
-      <SearchCEP 
-        {...this.state} 
-        {...this.props.address}
-        handleSubmit={this.handleSubmit} 
-        />
-    )
-  }
+const Search  = ({ address, handleSubmit }) => {
+  return (
+    <SearchCEP 
+      {...address}
+      handleSubmit={handleSubmit} 
+      />
+  )
 }
 
 const mapStateToProps = (state) => ({
@@ -51,7 +21,10 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  setAddress: (data) => dispatch(updateAddress(data))
-})
+  handleSubmit: (e) => {
+    e.preventDefault();
+    dispatch(fetchAddress(e.target.cep.value));
+  }
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
--- src/redux-flow/reducers/ZipCode/action-creator.js
+++ src/redux-flow/reducers/ZipCode/action-creator.js
@@ -1,4 +1,12 @@
+import axios from 'axios';
 import { UPDATE_ADDRESS } from './actions'
+
+export const fetchAddress = (cep) => 
+  async (dispatch, getState) => {
+    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+    dispatch(updateAddress(response.data));
+    // this.setState({ isLoading: false })
+  } 
 
 export const updateAddress = (data) => ({
   type: UPDATE_ADDRESS,
